d012eae183cc7676ac2794cf4369a17917288c65
ThinkingBbefore <59727047+ThinkingBbefore@users.noreply.github.com>
2020-01-16 16:23:50 +0500

    Add files via upload

---
+++ Task lesson2.py
@@ -0,0 +1,87 @@
+# #Task №1
+#
+# i = 0
+# for l in range(0,6):
+#     print(l, i)
+#
+# #Task №2
+#
+# sum = 0
+# for i in range(10):
+#     number = int(input())
+#     if number == 5:
+#         sum += number
+# print(int(sum / 5))
+
+# #Task №3
+#
+# sum = 0
+# for i in range(1, 101):
+#     sum += i
+# print(sum)
+
+# #Task №4
+
+# sum = 1
+#
+# for i in range(1, 11):
+#     sum *= i
+# print(sum)
+
+#Task №5
+
+# integer_number = 2129
+#
+# while integer_number>0:
+#     print(integer_number%10)
+#     integer_number = integer_number//10
+
+#Task №6
+
+# i = 2129
+# sum = 0
+#
+# while i > 0:
+#     print(i % 10)
+#     sum = sum + (i % 10)
+#     i = i // 10
+# print(sum)
+
+#Task №7
+
+# i = 2129
+# sum = 1
+#
+# while i > 0:
+#     print(i % 10)
+#     sum = sum * (i % 10)
+#     i = i // 10
+# print(sum)
+
+#Task №8
+#
+# integer_number = 213413
+# while integer_number>0:
+#     if integer_number%10 == 5:
+#         print('Yes')
+#         break
+#     integer_number = integer_number//10
+# else: print('No')
+
+# Task №10
+# i = 345543
+# sum = 0
+#
+# while i > 0:
+#     if i % 10 == 5:
+# #        print(i % 10)
+#         sum = sum + (i % 10 == 5)
+#     i = i // 10
+# print(sum)
+
+#Task №9
+'''
+
+Не могу понять как сравнивать полученные числа
+
+'''
